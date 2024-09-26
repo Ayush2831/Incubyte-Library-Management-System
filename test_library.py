1fa6ab5023c9302cd49ef402be64efa72e0eff2d
Nandan <nandnandanpatil@gmail.com>
2024-09-26 20:07:59 +0530

    Add tests for borrowing books

--- test_library.py
+++ test_library.py
@@ -17,5 +17,17 @@ class TestLibrary(unittest.TestCase):
         self.library.add_book(book)
         self.assertFalse(self.library.add_book(book))
 
+    def test_borrow_available_book(self):
+        book = Book("123456789", "Test Book", "Test Author", 2023)
+        self.library.add_book(book)
+        self.assertTrue(self.library.borrow_book("123456789"))
+
+    def test_borrow_unavailable_book(self):
+        book = Book("123456789", "Test Book", "Test Author", 2023)
+        self.library.add_book(book)
+        self.library.borrow_book("123456789")
+        self.assertFalse(self.library.borrow_book("123456789"))
+
+
 if __name__ == '__main__':
     unittest.main()
