111fea13997a6a118eefa52af99c743510e0cfe8
Nandan <nandnandanpatil@gmail.com>
2024-09-26 20:12:38 +0530

    Add tests for returning books

--- test_library.py
+++ test_library.py
@@ -28,6 +28,18 @@ class TestLibrary(unittest.TestCase):
         self.library.borrow_book("123456789")
         self.assertFalse(self.library.borrow_book("123456789"))
 
+    def test_return_borrowed_book(self):
+        book = Book("123456789", "Test Book", "Test Author", 2023)
+        self.library.add_book(book)
+        self.library.borrow_book("123456789")
+        self.assertTrue(self.library.return_book("123456789"))
+        self.assertFalse(book.is_borrowed)
+
+    def test_return_unborrowed_book(self):
+        book = Book("123456789", "Test Book", "Test Author", 2023)
+        self.library.add_book(book)
+        self.assertFalse(self.library.return_book("123456789"))
+
 
 if __name__ == '__main__':
     unittest.main()
